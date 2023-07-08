d17fc024f1ad1856883cda638801115df6e087d4
pasupol.bunsaen <niawkung@gmail.com>
2023-07-08 20:27:54 +0700

    update

--- src/Preview.js
+++ src/Preview.js
@@ -18,6 +18,7 @@ const FolderList = () => {
 
   useEffect(() => {
     fetchFolderData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchFiles = async (folderList) => {
