0dc7d1ca7745c4fdf87eabb632891f760bb851df
niawjunior <niawkung@gmail.com>
2023-07-09 14:23:19 +0700

    update layout

--- src/app/components/Preview.tsx
+++ src/app/components/Preview.tsx
@@ -1,22 +1,31 @@
 import Iframe from "react-iframe";
-import { BiLinkExternal, BiKnife, BiSolidGraduation, BiBomb } from 'react-icons/bi'
+import { BiLinkExternal, BiKnife, BiPlay, BiBomb } from 'react-icons/bi'
 interface FilesProps {
     folder: string;
     files: string[]
 }
 const Preview = ({ files }: { files: FilesProps[] }) => {
 
-    const handleClick = (folder: string, file: string) => {
+    const handleClickToCode = (folder: string, file: string) => {
         window.open(`https://github.com/peuan/css-battle/blob/main/public/css-battle/${folder}/${file}`, '_ blank');
+
+    }
+
+    const handleClickToPlay = (level: string) => {
+        window.open(`https://cssbattle.dev/play/${level}`, '_ blank');
 
     }
     const renderFolderCards = () => {
         return files.map((folder, index) => (
-            <div key={index} className="sm:flex gap-16 mt-10">
-                <h3 className="text-green-500 text-lg font-bold">
-                    LEVEL: {folder.folder}
-                </h3>
-                <div className="flex gap-4 flex-wrap justify-center ">
+            <div key={index} className="sm:flex-col gap-16 mt-10">
+                <div onClick={(() => handleClickToPlay(folder.folder))} className="px-2 cursor-pointer mb-5 w-fit">
+                    <h3 className="hover:bg-green-400 active:bg-green-400 focus:outline-none focus:ring focus:ring-blue-bg-yellow-400 flex justify-start items-center rounded-full bg-yellow-500 w-fit text-white text-lg font-bold py-1 px-4 text-md ">
+                        LEVEL: {folder.folder}
+                        <BiPlay className="ml-2 text-white" />
+                    </h3>
+                </div>
+
+                <div className="flex gap-4 flex-wrap justify-start ">
                     {renderFilePreviews(folder)}
                 </div>
             </div>
@@ -27,8 +36,8 @@ const Preview = ({ files }: { files: FilesProps[] }) => {
         return folder.files?.map((file, index) => {
             return (
                 <div key={index}>
-                    <div className="flex justify-center p-2">
-                        <div onClick={(() => handleClick(folder.folder, file))} className="rounded-full bg-blue-600 flex items-center px-2 cursor-pointer">
+                    <div className="flex justify-center mb-2">
+                        <div onClick={(() => handleClickToCode(folder.folder, file))} className="rounded-full bg-blue-600 flex items-center px-2 cursor-pointer">
                             <h3 className="text-white font-bold py-1 px-4 text-md ">
                                 {" "}
                                 {file.split("_")[0]}
@@ -56,8 +65,10 @@ const Preview = ({ files }: { files: FilesProps[] }) => {
                         <BiBomb className="ml-2" />
                     </div>
                 </h1>
-                <div className=" p-3 flex flex-col justify-center ">
-                    {renderFolderCards()}
+                <div className="flex justify-center">
+                    <div className="flex flex-col ">
+                        {renderFolderCards()}
+                    </div>
                 </div>
             </div>
         </>
