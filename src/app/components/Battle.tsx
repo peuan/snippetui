1369bfd7035978b2462c9d0377afb8cc5d523bc6
pasupol.bunsaen <niawkung@gmail.com>
2023-07-29 16:43:44 +0700

    update theme

--- src/app/components/Battle.tsx
+++ src/app/components/Battle.tsx
@@ -17,18 +17,18 @@ const Battle = ({ battleResults }: { battleResults: IBattleResult }) => {
             <div key={index} className="sm:flex-col mb-10">
               <div
                 onClick={() => handleClickToPlay(folder.folder)}
-                className="px-2 lg:mx-0 mx-8 mt-4 cursor-pointer mb-5 w-fit"
+                className="group px-2 lg:mx-0 mx-8 mt-4 cursor-pointer mb-5 w-fit hover:text-white"
               >
                 <h3
-                  className="hover:bg-green-400 active:bg-green-400
+                  className="hover:bg-slate-400 active:bg-slate-400 group-hover:text-white dark:hover:bg-green-400 active:dark:bg-green-400
                                     focus:outline-none focus:ring 
                                     focus:ring-blue-bg-yellow-400 
                                     flex justify-start items-center
-                                    rounded-full bg-yellow-500 w-fit 
-                                    text-white text-lg font-bold py-1 px-4 text-md"
+                                    rounded-full bg-slate-300 text-slate-800 dark:bg-yellow-500 w-fit 
+                                    dark:text-white text-lg font-bold py-1 px-4 text-md"
                 >
                   LEVEL: {folder.folder}
-                  <BiPlay className="ml-2 text-white" />
+                  <BiPlay className="group-hover:text-white ml-2 text-slate-800 dark:text-white" />
                 </h3>
               </div>
 
--- src/app/components/ResizeHandle.tsx
+++ src/app/components/ResizeHandle.tsx
@@ -1,4 +1,5 @@
 import { PanelResizeHandle } from "react-resizable-panels"
+import clsx from "clsx"
 
 import styles from "@/styles.module.css"
 
@@ -15,7 +16,10 @@ export default function ResizeHandle({
       id={id}
     >
       <div className={styles.ResizeHandleInner}>
-        <svg className={styles.Icon} viewBox="0 0 24 24">
+        <svg
+          className={clsx(`${styles.Icon} dark:text-white text-slate-800`)}
+          viewBox="0 0 24 24"
+        >
           <path
             fill="currentColor"
             d="M8,18H11V15H2V13H22V15H13V18H16L12,22L8,18M12,2L8,6H11V9H2V11H22V9H13V6H16L12,2Z"
