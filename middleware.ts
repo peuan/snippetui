1ffc15c245038d2d4d2aff4e7f4955a766a1b8c6
niawjunior <niawkung@gmail.com>
2023-10-14 20:51:18 +0700

    chore: update footer and remove unused code

--- middleware.ts
+++ middleware.ts
@@ -15,10 +15,5 @@ export default withAuth({
 })
 
 export const config = {
-  matcher: [
-    "/playground/:path*",
-    "/profile",
-    "/leaderboards",
-    "/discussions/:path*",
-  ],
+  matcher: ["/playground/:path*", "/profile", "/discussions/:path*"],
 }
